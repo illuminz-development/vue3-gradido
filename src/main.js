71318a18b68012e0877576e7d9afefc59bb3f528
Devinder Kumar <devinderkumar@illuminz.com>
2023-11-22 15:16:05 +0530

    remove g key

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
 import setupInterceptors from './services/setupInterceptors';
-import VueGoogleMaps from '@fawmi/vue-google-maps';
+// import VueGoogleMaps from '@fawmi/vue-google-maps';
 import VueSimpleAlert from "vue3-simple-alert-next";
 
 setupInterceptors(router);
@@ -15,12 +15,12 @@ const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(VueSimpleAlert);
-appInstance.use(VueGoogleMaps, {
-    load: {
-        key: process.env.VUE_APP_GOOGLE_MAP_KEY,
-        libraries: "places"
-        // language: 'de',
-    },
-});
+// appInstance.use(VueGoogleMaps, {
+//     load: {
+//         key: process.env.VUE_APP_GOOGLE_MAP_KEY,
+//         libraries: "places"
+//         // language: 'de',
+//     },
+// });
 appInstance.use(ArgonDashboard);
 appInstance.mount("#app");
